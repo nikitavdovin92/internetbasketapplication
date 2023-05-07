d7b4d91500fb3c014e2970bd9e71b938194eac49
Homework3 <tradervdovin@gmail.com>
2023-05-07 17:57:37 +0500

    1

---
+++ src/main/java/com/example/internetbasketapplication/InternetBasketApplication.java
@@ -0,0 +1,13 @@
+package com.example.internetbasketapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBasketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetBasketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/internetbasketapplication/InternetBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internetbasketapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBasketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
